ad9193c8416f87a7a3ae037163d2e5df346cb186
zhangshexin <zhangshexin@anne.com.cn>
2018-12-10 14:51:45 +0800

    1、test

--- app/src/main/java/com/redread/Activity_home.java
+++ app/src/main/java/com/redread/Activity_home.java
@@ -42,6 +42,7 @@ public class Activity_home extends BaseActivity implements ViewPager.OnPageChang
     private HomePageAdapter adapter;
     private List<Fragment> fragments=new ArrayList<>();
     private void initView() {
+
         Fragment_publish fragmentPublish=new Fragment_publish();
         Fragment_libary fragmentLibary=new Fragment_libary();
         Fragment_Booktrack fragmentBooktrack=new Fragment_Booktrack();
